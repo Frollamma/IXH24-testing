72ab77535c34f91d9bb01d7b0de51658a4cc4017
Frollamma <fral01@protonmail.com>
2024-11-20 22:54:09 +0100

    Improve lesson 1 of XRLP ledger

--- Quickstart Tutorial by XRPL website/mod1.py
+++ Quickstart Tutorial by XRPL website/mod1.py
@@ -2,36 +2,48 @@ import xrpl
 
 testnet_url = "https://s.devnet.rippletest.net:51234/"
 
+
 def get_account(seed):
     """get_account"""
     client = xrpl.clients.JsonRpcClient(testnet_url)
-    if (seed == ''):
+    if seed == "":
         new_wallet = xrpl.wallet.generate_faucet_wallet(client)
     else:
         new_wallet = xrpl.wallet.Wallet.from_seed(seed)
     return new_wallet
 
+
 def get_account_info(accountId):
     """get_account_info"""
     client = xrpl.clients.JsonRpcClient(testnet_url)
     acct_info = xrpl.models.requests.account_info.AccountInfo(
-        account=accountId,
-        ledger_index="validated"
+        account=accountId, ledger_index="validated"
     )
     response = client.request(acct_info)
-    return response.result['account_data']
+    return response.result["account_data"]
+
+
+class MockResponse:
+    pass
+
 
 def send_xrp(seed, amount, destination):
     sending_wallet = xrpl.wallet.Wallet.from_seed(seed)
     client = xrpl.clients.JsonRpcClient(testnet_url)
+
+    print(f"Sending {amount} XRP from {sending_wallet.address} to {destination}")
+
     payment = xrpl.models.transactions.Payment(
         account=sending_wallet.address,
-        amount=xrpl.utils.xrp_to_drops(int(amount)),
+        # amount=xrpl.utils.xrp_to_drops(int(amount)),
+        amount=str(amount),
         destination=destination,
     )
-    try:	
-        response = xrpl.transaction.submit_and_wait(payment, client, sending_wallet)	
-    except xrpl.transaction.XRPLReliableSubmissionException as e:	
-        response = f"Submit failed: {e}"
+    try:
+        response = xrpl.transaction.submit_and_wait(payment, client, sending_wallet)
+    except xrpl.transaction.XRPLReliableSubmissionException as e:
+        # Return a response object with result field set to the error message
+        response = MockResponse()
+        response.result = e
 
     return response
