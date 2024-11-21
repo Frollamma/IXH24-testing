b19d3d775292fade962cc4e317fbe58eb1a44f6b
Frollamma <fral01@protonmail.com>
2024-11-21 10:31:11 +0100

    Partially fix conditional escrows file

--- utils.py
+++ utils.py
@@ -1,13 +1,26 @@
 from xrpl.account import get_balance
 from xrpl.clients import JsonRpcClient
+from xrpl.wallet import Wallet, generate_faucet_wallet
 from xrpl.core import keypairs
 
 
-def generate_xrpl_wallet_seed() -> str:
+def _generate_xrpl_wallet_seed() -> str:
     """
     Generate a random seed. Notice that if you create a wallet with this seed, it won't be a working wallet, because you need to fund it before. See https://xrpl.org/docs/concepts/accounts#creating-accounts.
     """
     return keypairs.generate_seed()
+
+
+def get_wallet(seed: str | None = None) -> Wallet:
+    """
+    Generate a wallet.
+    """
+    client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+    if seed:
+        wallet = Wallet.from_seed(seed)
+    else:
+        wallet = generate_faucet_wallet(client, debug=True)
+    return wallet
 
 
 def print_balances(wallets: list, client: JsonRpcClient) -> None:
